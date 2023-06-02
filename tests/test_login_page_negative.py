2424dfc35d6b68c062ddc3fa7e1b9f16fb13100c
Vitaly S <vitaliksew@gmail.com>
2023-06-02 19:24:12 -0400

    add: requirements.txt

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -8,7 +8,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class TestNegativeScenarios:
-
+	@pytest.mark.login
+	@pytest.mark.negative
 	def test_negative_username(self):
 		driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 		# time.sleep(2)
@@ -31,8 +32,34 @@ class TestNegativeScenarios:
 
 		error_text = driver.find_element(By.ID, "error")
 		print(error_text.text)
-		assert error_text.text == 'Your username is invalid!'
+		assert error_text.is_displayed() and error_text.text == 'Your username is invalid!'
 
+
+	@pytest.mark.login
+	@pytest.mark.negative
+	def test_negative_password(self):
+		driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+		# time.sleep(2)
+		# Open page
+		driver.get("https://practicetestautomation.com/practice-test-login/")
+		# time.sleep(2)
+		# Type username student into Username field
+		username_locator = driver.find_element(By.ID, "username")
+		# time.sleep(2)
+		username_locator.send_keys("student")
+		# Type password incorrectPassword into Password field
+		password_locator = driver.find_element(By.NAME, "password")
+		# time.sleep(2)
+		password_locator.send_keys("incorrectPassword")
+		# Push Submit button
+		submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+		# time.sleep(2)
+		submit_button_locator.click()
+		time.sleep(2)
+
+		error_text = driver.find_element(By.ID, "error")
+		print(error_text.text)
+		assert error_text.is_displayed() and error_text.text == 'Your password is invalid!'
 # Open page
 # Type username incorrectUser into Username field
 # Type password Password123 into Password field
