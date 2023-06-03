fa5fc7316690db04d7ecf17ef1059aa7d78c36a9
Vitaly S <vitaliksew@gmail.com>
2023-06-02 23:20:45 -0400

    add: several browsers in fixture in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,27 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
-@pytest.fixture()
-def driver():
-    print("creating chrome driver")
-    my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+@pytest.fixture(params=["chrome", "firefox"])
+def driver(request):
+    # browser = request.config.getoption("--browser")
+    browser = request.param
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox', but got {browser}")
     yield my_driver
-    print("closing chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)"
+    )
